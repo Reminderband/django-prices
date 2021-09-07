d11c8adc839bf28af9f07ffa1ea7633a7613c273
Chad Saunders <chadsaun@gmail.com>
2021-09-07 14:18:51 -0600

    Django - Make compatible with v3

--- django_prices/models.py
+++ django_prices/models.py
@@ -39,7 +39,7 @@ class PriceField(BaseField):
         super(PriceField, self).contribute_to_class(cls, name, **kwargs)
         setattr(cls, self.name, Creator(self))
 
-    def from_db_value(self, value, expression, connection, context):
+    def from_db_value(self, value, expression, connection):
         return self.to_python(value)
 
     def to_python(self, value):
